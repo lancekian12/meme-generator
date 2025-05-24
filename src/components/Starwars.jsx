b60bdb64266b452153fd836bf96ca516dbba5184
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-25 04:55:40 +0800

    use dependency array to fix infinite rendering

--- src/components/Starwars.jsx
+++ src/components/Starwars.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 
 const Starwars = () => {
     const [starWarsData, setStarWarsData] = React.useState(null)
+    const [count, setCount] = React.useState(0)
+
+    console.log("Rendered!")
 
     React.useEffect(() => {
+        console.log("Effect ran")
         fetch("https://swapi.dev/api/people/1")
             .then(res => res.json())
             .then(data => setStarWarsData(data))
-    })
+    }, [count])
 
 
     return (
         <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         </div>
     )
